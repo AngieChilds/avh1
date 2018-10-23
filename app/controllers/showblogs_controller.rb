870e5e042928c9cff4bf143ff2e29c6db72f1c20
Angie <pirate.princess.web@gmail.com>
2018-10-23 22:13:54 +0200

    fix showblog destroy

--- app/controllers/showblogs_controller.rb
+++ app/controllers/showblogs_controller.rb
@@ -1,7 +1,7 @@
 class ShowblogsController < ApplicationController
  before_action :logged_in_user, only: [:index, :new, :edit, :update, :destroy]
  before_action :trusty_user,    only: [:new, :edit, :update, :destroy]
-
+ before_action :moderator_user, only: [:edit, :update, :destroy]
 
    def create
     @showblog = Showblog.new(showblog_params)
